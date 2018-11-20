804dbab2805356066b5eed9bd852a1ffbde7444e
Juan Borrajo <juanborrajo10@gmail.com>
2018-11-20 12:51:05 +0100

    Inicio project

---
+++ src/ejercicio5/Ejercicio5.java
@@ -0,0 +1,15 @@
+
+package ejercicio5;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class Ejercicio5 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
